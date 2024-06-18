d74c1559d929e00d7f7fa158e511d62e7a0ecebe
Žilvinas <159907476+ZilvinasSP@users.noreply.github.com>
2024-06-18 14:22:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ username = 'your_username'
 password = 'your_password'
 endpoint = 'gate.smartproxy.com'
 port = '7000'
-website = 'https://www.ipinfo.io/ip'
+website = 'https://ip.smartproxy.com/json'
 
 chrome_options = webdriver.ChromeOptions()
 
@@ -16,6 +16,8 @@ proxies_extension = proxies(username, password, endpoint, port)
 chrome_options.add_extension(proxies_extension)
 chrome_options.add_argument("--headless=new")
 
-
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 chrome.get(website)
+
+print(chrome.page_source)
+chrome.quit()
